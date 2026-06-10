f501ccd7d01e82c07d84ad6a218891e8304fcd68
Ross Golder <ross@golder.org>
2026-06-10 12:14:41 +0700

    fix: regenerate CRDs with current controller-gen

--- apis/instance/v1beta1/zz_generated.deepcopy.go
+++ apis/instance/v1beta1/zz_generated.deepcopy.go
@@ -21,7 +21,7 @@ limitations under the License.
 package v1beta1
 
 import (
-	v1 "github.com/crossplane/crossplane/apis/v2/core/v2"
+	"github.com/crossplane/crossplane/apis/v2/core/v2"
 	runtime "k8s.io/apimachinery/pkg/runtime"
 )
 
@@ -127,7 +127,7 @@ func (in *InstanceParameters) DeepCopyInto(out *InstanceParameters) {
 	}
 	if in.RootPasswordSecretRef != nil {
 		in, out := &in.RootPasswordSecretRef, &out.RootPasswordSecretRef
-		*out = new(v1.SecretKeySelector)
+		*out = new(v2.SecretKeySelector)
 		**out = **in
 	}
 }
